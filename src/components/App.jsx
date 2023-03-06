2c3c32f5ee60de6a0535bdeaaa8d031792eb0047
Eugene8615 <reznichenko8615@gmail.com>
2023-03-07 00:18:37 +0200

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,57 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import {Component} from "react";
+import SectionTitle from "./Section/SectionTitle";
+import ContactForm from "./ContactForm/ContactForm";
+import Section from "./Section/Section";
+import Filter from "./Filter/Filter";
+import ContactsList from "./ContactsList/ContactsList";
+
+export class App extends Component{
+
+  state = {
+    contacts: [],
+    filter: '',
 };
+
+handleAddContact = (newContact) => {
+    const { contacts } = this.state;
+
+    const alertContact = contacts.find(contact => contact.name === newContact.name)
+
+    if (alertContact) {
+        alert(`${newContact.name} is already in contacts`)
+        return;
+    } else {
+        this.setState(prevState => ({
+            contacts: [...prevState.contacts, newContact]
+        }));
+    }
+}
+
+handleDeleteContact = (contactID) => {
+    this.setState(prevState => {
+        return { contacts: prevState.contacts.filter(contact => contact.contactID !== contactID) }
+    })
+}
+
+handleFilter = evt => {
+    this.setState({ filter: evt.target.value });
+};
+
+  render() {
+    const { contacts, filter } = this.state;
+    return (
+      <div>
+        <SectionTitle title="Phonebook">
+         <ContactForm addContact={this.handleAddContact} />
+         </SectionTitle>
+
+         <Section subTitle="Contacts">
+             <Filter onChange={this.handleFilter} />
+             <ContactsList list={contacts} search={filter} deleteContact={this.handleDeleteContact} />
+         </Section>
+      </div>
+    )
+  }
+}
+
+export default App;
